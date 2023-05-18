a7803bf1e7509628bb545f0a9ad2585a571940e4
Huy Ong <ogh.huy@gmail.com>
2023-05-18 08:30:20 -0700

    Initial Commit

---
+++ Senti/SentiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentiApp.swift
+//  Senti
+//
+//  Created by Huy Ong on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SentiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
